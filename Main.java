fe5d317915534025f4dd43bf7e7dec008e3a29e8
Marshmellowon <leo503801@gmail.com>
2020-04-13 00:52:06 +0900

    좌표받기

--- Main.java
+++ Main.java
@@ -7,11 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        int n = sc.nextInt(); /* n개의 점 */
 
-        `
+        /* 점 입력 받기 */
+        int[][] s = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            s[i][0] = sc.nextInt();
+            s[i][1] = sc.nextInt();
+            System.out.println(s[i][0] + " " + s[i][1]);
+        }
+
+        if (n <= 3) {
+
+        }
 
     }
 }
