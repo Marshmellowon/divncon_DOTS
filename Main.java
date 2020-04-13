0137b1c317d7ddb9d4cf01f5baa06e32bc3b32ce
Marshmellowon <leo503801@gmail.com>
2020-04-13 18:59:52 +0900

    Point class

--- Main.java
+++ Main.java
@@ -1,25 +1,52 @@
 import java.util.Scanner;
+import java.awt.*;
 
 /**
  * Main
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static double Dist(double d, double f, double d1, double f1) {
+        double res = Math.sqrt(Math.pow(Math.abs(d1 - d), 2) + Math.pow(Math.abs(f1 - f), 2));
+        return res;
+    }
+
+    public void Dots() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); /* n개의 점 */
 
+        int left = 0;
+        int right = 0;
+
+        Point p1 = new Point(2, 3);
         /* 점 입력 받기 */
-        int[][] s = new int[n][2];
+        double[][] s = new double[n][2];
         for (int i = 0; i < n; i++) {
-            s[i][0] = sc.nextInt();
-            s[i][1] = sc.nextInt();
+            s[i][0] = sc.nextDouble();
+            s[i][1] = sc.nextDouble();
+
             System.out.println(s[i][0] + " " + s[i][1]);
+
+            if (n <= 3) {
+
+            }
+            if (n % 2 == 0) {
+                int mid1 = n / 2;
+
+            } else {
+                int mid2 = 0;
+
+            }
+            /* System.out.println(s[0][0] + "" + s[0][1] + "" + s[1][0] + "" + s[1][2]); */
+            /*
+             * double pri = Dist(s[0][0], s[0][1], s[1][0], s[1][2]);
+             * System.out.println(pri);
+             */
         }
+    }
 
-        if (n <= 3) {
-
-        }
-
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.Dots();
     }
 }
